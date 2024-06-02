e8e1eef53624ce606d49378f993365193d88c293
Bill Sorensen <william.sorensen@grafana.com>
2024-06-02 15:04:58 -0600

    -Initial test example and config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,27 @@
-import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
+  afterEach(cleanup);
+
   it("should render", () => {
     render(<App />);
 
     expect(
       screen.getByText("React Testing Library antipatterns"),
     ).not.toBeNull();
+  });
+
+  it("should be findable", () => {
+    render(<App />);
+
+    expect(screen.getByTestId("find-me")).not.toBeNull();
+  });
+
+  it("should be findable", () => {
+    render(<App />);
+
+    expect(screen.getByTestId("find-me")).not.toBeNull();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1>React Testing Library antipatterns</h1>
+      <div data-testid="find-me">Not particularly accessible</div>
     </>
   );
 }
