d852099facb3675a551c8f719c5216e188a87a78
Bill Sorensen <william.sorensen@grafana.com>
2024-06-28 12:17:30 -0600

    Add testing-library ESLint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import testingLibrary from "eslint-plugin-testing-library";
 
 export default [
   {
@@ -20,6 +21,43 @@ export default [
     },
     rules: {
       "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    files: ["**/*.[jt]s?(x)"],
+    plugins: {
+      "testing-library": testingLibrary,
+    },
+    rules: {
+      // Gave up on extending recommended React rules with ESLint flat config.
+      "testing-library/await-async-events": [
+        "error",
+        { eventModule: "userEvent" },
+      ],
+      "testing-library/await-async-queries": "error",
+      "testing-library/await-async-utils": "error",
+      "testing-library/no-await-sync-events": [
+        "error",
+        { eventModules: ["fire-event"] },
+      ],
+      "testing-library/no-await-sync-queries": "error",
+      "testing-library/no-container": "error",
+      "testing-library/no-debugging-utils": "warn",
+      "testing-library/no-dom-import": ["error", "react"],
+      "testing-library/no-global-regexp-flag-in-query": "error",
+      "testing-library/no-manual-cleanup": "error",
+      "testing-library/no-node-access": "error",
+      "testing-library/no-promise-in-fire-event": "error",
+      "testing-library/no-render-in-lifecycle": "error",
+      "testing-library/no-unnecessary-act": "error",
+      "testing-library/no-wait-for-multiple-assertions": "error",
+      "testing-library/no-wait-for-side-effects": "error",
+      "testing-library/no-wait-for-snapshot": "error",
+      "testing-library/prefer-find-by": "error",
+      "testing-library/prefer-presence-queries": "error",
+      "testing-library/prefer-query-by-disappearance": "error",
+      "testing-library/prefer-screen-queries": "error",
+      "testing-library/render-result-naming-convention": "error",
     },
   },
 ];
