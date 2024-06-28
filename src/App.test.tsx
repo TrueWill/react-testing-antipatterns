9db4d7b0b787424fe5f1fc3ccd750b2b9eb11f8b
Bill Sorensen <william.sorensen@grafana.com>
2024-06-28 11:19:31 -0600

    Improve and add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,45 @@
 import { afterEach, describe, expect, it } from "vitest";
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { act, useEffect, useState } from "react";
+
+global.IS_REACT_ACT_ENVIRONMENT = false;
 
 describe("App", () => {
   afterEach(cleanup);
+
+  describe("Inaccessible", () => {
+    it("should be findable", () => {
+      render(<App />);
+
+      expect(screen.getByTestId("find-me")).not.toBeNull();
+    });
+
+    it("should be queryable", () => {
+      const { container } = render(<App />);
+
+      container.querySelector(".my-class");
+
+      expect(container.querySelector(".my-class")).not.toBeNull();
+    });
+  });
+
+  describe("Act", () => {
+    it("should not need act", async () => {
+      render(<App />);
+
+      const button = screen.getByRole("button", { name: /click me/i });
+
+      // I tried to create an example that would give the "not wrapped in act" warning
+      // but never saw it.
+
+      act(() => {
+        fireEvent.click(button);
+      });
+
+      expect(await screen.findByText("done")).not.toBeNull();
+    });
+  });
 
   it("should render", () => {
     render(<App />);
@@ -11,17 +47,5 @@ describe("App", () => {
     expect(
       screen.getByText("React Testing Library antipatterns"),
     ).not.toBeNull();
-  });
-
-  it("should be findable", () => {
-    render(<App />);
-
-    expect(screen.getByTestId("find-me")).not.toBeNull();
-  });
-
-  it("should be findable", () => {
-    render(<App />);
-
-    expect(screen.getByTestId("find-me")).not.toBeNull();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [status, setStatus] = useState("");
+
+  async function handleClick() {
+    setStatus("pending");
+    await sleep(500);
+    setStatus("done");
+  }
+
   return (
     <>
       <h1>React Testing Library antipatterns</h1>
-      <div data-testid="find-me">Not particularly accessible</div>
+      <div className="my-class" data-testid="find-me">
+        Not particularly accessible
+      </div>
+      <button type="button" onClick={handleClick}>
+        Click me
+      </button>
+      {status}
     </>
   );
 }
 
 export default App;
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
